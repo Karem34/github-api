f3a92ae965aaa0e07502b24b607b8222665eccd8
Karem34 <karemjaleel34@gmail.com>
2021-02-17 02:05:37 +0300

    second commit

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
 const showData = async () => {
   let lang = document.getElementById('lang-input').value
   let license = document.getElementById('license-input').value
+  let search = document.getElementById('search-input').value
+  let stars = document.getElementById('stars-input').value
+  let url = 'https://api.github.com/search/repositories?q=';
+  let name = document.getElementById('Name')
 
-  console.log('fetching data')
-  let url = 'https://api.github.com/search/repositories?q=license:MIT';
-  if (lang !== '') {
-    url = url + '+language:' + lang
-  }
+  if (search !== '') {url += search}
+  if (stars !== '') {url += '+stars:>=' + stars}
+  if (lang !== '') {url += '+language:' + lang}
+  if (license !== '') {url += '+license:' + license}
+
   const r = await fetch(url);
   data = await r.json();
+
   let rand_item = Math.floor(Math.random() * data['items'].length)
   repo = data['items'][rand_item]
-  console.log(repo)
-  let name = document.getElementById('Name')
+
   let full_name = repo['full_name']
   name.innerText = full_name
   name.setAttribute('href', "http://github.com/" + full_name);
   document.getElementById('Stars').innerText = repo['stargazers_count']
   document.getElementById('Forks').innerText = repo['forks_count']
-  document.getElementById('Watchers').innerText = repo['watchers']
-  document.getElementById('Lang').innerText = repo['language']
-}
+  document.getElementById('Description').innerText = repo['description']
 
+  if (repo['language'] == null) {
+    document.getElementById('Lang').innerText = "No language"
+    }else{
+    document.getElementById('Lang').innerText = repo['language']
+  }
+  if (repo['license'] == null) {
+    document.getElementById('License').innerText = "No License"
+    }else{
+    document.getElementById('License').innerText = repo['license']['name']
+  }
+}
 showData()
